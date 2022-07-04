9e78e7d81bd391eb57995dce29ec363e0df11e00
Andrew TOwnsley <atownsley86@gmail.com>
2022-07-03 21:39:47 -0600

    Creating Vomponents, passoing parameters

---
+++ HotelBlazor_Server/Model/Room.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelBlazor_Server.Model
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        public string RoomName { get; set; }
+
+        public double Price { get; set; }
+
+        public bool IsBooked{ get; set; }
+
+        public List<RoomProp> RoomProps { get; set; }
+
+    }
+}
---
+++ HotelBlazor_Server/Model/RoomProp.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBlazor_Server.Model
+{
+    public class RoomProp
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+    }
+}
