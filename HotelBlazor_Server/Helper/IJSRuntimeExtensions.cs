f8dafa531926ba598dd0afe5791958e0e9cedb8e
Andrew TOwnsley <atownsley86@gmail.com>
2022-07-12 11:04:24 -0600

    JS alerts

---
+++ HotelBlazor_Server/Helper/IJSRuntimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace HotelBlazor_Server.Helper
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime JsRuntime, string message)
+        {
+            await JsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        public static async ValueTask ToastrError(this IJSRuntime JsRuntime, string message)
+        {
+            await JsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
---
+++ HotelBlazor_Server/wwwroot/js/common.js
@@ -0,0 +1,26 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'TOASTR SUCCESS MESSAGE!!!');
+    } else if
+    (type === "error") {
+        toastr.error(message, "ERROR MESSAGE FROM TOASTR!!!");
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire({
+            title: "Success",
+            text: "Success!",
+            icon: "success",
+            confirmButtonText: "Ok, I guess"
+        });
+    } else if (type === "error") {
+        Swal.fire({
+            title: "Error",
+            text: "Error has been logged!",
+            icon: "error",
+            confirmButtonText: "Ok, I guess"
+        });
+    }
+}
